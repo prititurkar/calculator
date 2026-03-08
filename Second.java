aac5b7825feea2ea2922c089ccea2f8520e3df53
priti turkar <preetiturkrar814@gmail.com>
2026-03-08 18:47:24 +0530

    modified:   First.java         modified:   Second.java

--- Second.java
+++ Second.java
@@ -1,33 +1,56 @@
-import java.util.*;
+// import java.util.*;
 
-public class Second {
-    public static void main(String args[]) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter no.");
-        int a = sc.nextInt();
-        char  num = sc.next().charAt(0);
-        int b = sc.nextInt();
+// public class Second {
+//     public static void main(String args[]) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("Enter no.");
+//         int a = sc.nextInt();
+//         char  num = sc.next().charAt(0);
+//         int b = sc.nextInt();
         
 
-        if(num == '+'){
-            System.out.println(a+b);
-        }
-        else if(num == '-'){
-              System.out.println(a-b);
-        }
-        else if(num == '*'){
-            System.out.println(a*b);
-        }
-        else if(num == '/'){
-            System.out.println(a/b);
+//         if(num == '+'){
+//             System.out.println(a+b);
+//         }
+//         else if(num == '-'){
+//               System.out.println(a-b);
+//         }
+//         else if(num == '*'){
+//             System.out.println(a*b);
+//         }
+//         else if(num == '/'){
+//             System.out.println(a/b);
 
-        }
-        else if(num == '%'){
-            System.out.println(a%b);
-        }
-        else{
-            System.out.println("invalid no.");
-        }
-        }      
+//         }
+//         else if(num == '%'){
+//             System.out.println(a%b);
+//         }
+//         else{
+//             System.out.println("invalid no.");
+//         }
+//         }      
               
-         }
+//          }
+
+
+
+
+
+
+
+
+// public class First {
+
+//     public static void main(String args[]) {
+//         int num = 1;
+//         while (num <= 10) {
+//             if (num == 5) {
+
+//                 break;
+//             }
+//             System.out.println(num);
+//             num++;
+//         }
+//     }
+
+// }
