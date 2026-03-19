08a157c0fd37092613038c72b2fe4f28ffe9d25f
priti turkar <preetiturkrar814@gmail.com>
2026-03-19 12:54:15 +0530

    modified:   Array.java         new file:   Strings.java

--- Array.java
+++ Array.java
@@ -272,20 +272,22 @@
 // }
 
 //reverse Array
-import java.util.*;
-public class Array{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] num = new int[size];
-        for(int i=0; i<size; i++){
-            num[i]=sc.nextInt();
+// import java.util.*;
+// public class Array{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int[] num = new int[size];
+//         for(int i=0; i<size; i++){
+//             num[i]=sc.nextInt();
 
-        }
+//         }
 
-        for(int i=size-1; i>=0; i--){
-            System.out.println(num[i]);
+//         for(int i=size-1; i>=0; i--){
+//             System.out.println(num[i]);
 
-        }
-    }
-}
+//         }
+//     }
+// }
+
+
---
+++ Strings.java
@@ -0,0 +1,33 @@
+
+//concatenation
+// public class Strings {
+
+//     public static void main(String args[]){
+//         String firstName = "priti";
+//         String lastName = "turkar";
+//         String fullName = firstName+ " "+lastName;
+//         System.out.println(fullName);
+//     }
+    
+// }
+
+//length();
+
+// public class Strings{
+//     public static void main(String args[]){
+//         String name = "My name is priti";
+//         System.out.println(name.length());
+//     }
+// }
+
+//charAt
+
+public class Strings{
+    public static void main(String args[]){
+    String name = "my name is priti";
+    for(int i=0; i<name.length(); i++){
+        System.out.println(name.charAt(i));
+    }
+    
+}
+}
